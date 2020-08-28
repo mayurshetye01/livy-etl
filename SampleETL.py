a7a90a579bd44acd0afe544c9edebe6b0fc5d0ee
Mayur <63654367+mayurshetye01@users.noreply.github.com>
2020-08-29 03:09:37 +0530

    Update SampleETL.py

--- SampleETL.py
+++ SampleETL.py
@@ -4,14 +4,13 @@ from pyspark.sql import SparkSession
 class SampleETL:
     # TODO-> Use file from livy 'files' option
     input_file = "/season-1819.csv"
-    output_file = "/code/man_united_games.csv"
+    output_file = "/man_united_games.csv"
 
     spark = SparkSession.builder.getOrCreate()
     # Create the DataFrame
     df = spark.read.format('csv').option("header", "true").load(input_file)
 
     filtered_df = df.filter(df['HomeTeam'] == 'Man United')
-    filtered_df.show()
 
     filtered_df.createOrReplaceTempView('man_utd_games')
 
@@ -20,7 +19,5 @@ class SampleETL:
                                'FTHG as goals_for, FTAG as goals_against'
                                'from man_utd_games')
     result_df.write.csv(output_file)
-
-    print('Data exported successfully')
 
     result_df.show()
