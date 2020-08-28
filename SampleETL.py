d42eb8663a605ed470abeba78f49a15384a7277c
Mayur <63654367+mayurshetye01@users.noreply.github.com>
2020-08-29 03:03:25 +0530

    Update SampleETL.py

--- SampleETL.py
+++ SampleETL.py
@@ -10,17 +10,4 @@ class SampleETL:
     # Create the DataFrame
     df = spark.read.format('csv').option("header", "true").load(input_file)
 
-    filtered_df = df.filter(df['HomeTeam'] == 'Man United' | df['AwayTeam'] == 'Man United')
-    filtered_df.show()
-
-    filtered_df.createOrReplaceTempView('man_utd_games')
-
-    result_df = spark.sql('select '
-                               'HomeTeam as home, AwayTeam as away, '
-                               'FTHG as goals_for, FTAG as goals_against'
-                               'from man_utd_games')
-    result_df.write.csv(output_file)
-
-    print('Data exported successfully')
-
-    result_df.show()
+    df.show()
