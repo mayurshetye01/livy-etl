9f3fc5a42dc01323e1ea679583e317907948f08a
Mayur <63654367+mayurshetye01@users.noreply.github.com>
2020-08-29 02:18:40 +0530

    Update SampleETL.py

--- SampleETL.py
+++ SampleETL.py
@@ -2,18 +2,18 @@ from pyspark.sql import SparkSession
 
 
 class SampleETL:
-    base_dir = "/code/"
-    input_file = base_dir + "data.csv"
-    output_file = base_dir + "man_united_games.csv"
-
-    sqlContext = SparkSession.builder.getOrCreate()
+    input_file = "season-1819.csv"
+    output_file = "/code/man_united_games.csv"
 
     # Create the DataFrame
-    df = sqlContext.read.csv(input_file)
+    df = sc.read.csv(input_file)
 
     filtered_df = df.filter(df['HomeTeam'] == 'Man United' or df['AwayTeam'] == 'Man United')
+    filtered_df.show()
+
     filtered_df.createOrReplaceTempView('man_utd_games')
 
+    sqlContext = SparkSession.builder.getOrCreate()
     result_df = sqlContext.sql('select '
                                'HomeTeam as home, AwayTeam as away, '
                                'FTHG as goals_for, FTAG as goals_against'
